edf921959eec09f3156ae540f9e365117367ffd8
dream10201 <201269807@qq.com>
2021-03-05 09:58:16 +0800

    add release

---
+++ release.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+set -e
+REPO=$(cd $(dirname $0); pwd)
+
+checkParam () {
+  echo "👀 Checking semver format"
+
+  if [ $# -ne 1 ]; then
+    echo "❌ This release script requires a single argument corresponding to the semver to be released. See semver.org"
+    exit 1
+  fi
+
+  GREP="grep"
+  if [ -x "$(command -v ggrep)" ]; then
+    GREP="ggrep"
+  fi
+
+  semver=$(echo "$1" | $GREP -P '^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)')
+
+  if [ $? -ne 0 ]; then
+    echo "❌ Not valid semver format. See semver.org"
+    exit 1
+  fi
+}
+release () {
+  cd $REPO
+
+  checkParam $1
+
+  echo "🧼  Tidying up go modules"
+  go mod tidy
+
+  echo "🐑 Creating a new commit for the new release"
+  git commit --allow-empty -am "chore: version $semver"
+  git tag "$1"
+  git push
+  git push --tags origin
+
+  echo "📦 Done! $semver released."
+}
+
+while getopts "bacr:d" o; do
+  case "${o}" in
+    r)
+      release
+      ;;
+    a)
+      ASSETS="true"
+      ;;
